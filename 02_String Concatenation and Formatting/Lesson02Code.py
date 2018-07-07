86a1450a037752f8fb56ec14e2083f74d1f20708
SaretMagnoslove <magnoslove@gmail.com>
2018-07-07 18:13:11 +0300

    Add Lesson03 Code

--- 02_String Concatenation and Formatting/Lesson02Code.py
+++ 02_String Concatenation and Formatting/Lesson02Code.py
@@ -1,19 +1,29 @@
 names = ['Londo', 'Sheridan', 'Ivanova', 'Vir']
 
-# for name in names:
-#     # redability
-#     print('Hello there, ', name)
-#     # preformance
-#     print(' '.join(['hello there, ', name]))
-#
-# print(', '.join(names))
+for name in names:
+    # redability
+    print('Hello there, ', name)
+    # preformance
+    print(' '.join(['hello there, ', name]))
+
+print(', '.join(names))
 
 import os
-file_location = r"E:\Courses\youtube\-Intermediate_Python_Programming-sentdex"
+file_location = 'E:\\Courses\\youtube\\-Intermediate_Python_Programming-sentdex'
 file_name = 'example.txt'
 
+# # tempting
+# print(file_location + '\\' + file_name)
+# # right way
+# with open(os.path.join(file_location,file_name)) as f:
+#     print(f.read())
+
+who = 'Londo'
+how_many = 12
+
+# we want Londo bought 12 apples today
+
 # tempting
-test = (file_location + r"\\" + file_name)
+print(who, 'bought', how_many, 'apples today')
 # right way
-with open(test) as f:
-    print(f.read())
+print('{1} bought {0} apples today!'.format(who, how_many))
