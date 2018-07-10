cd8c1e24b2ab4b6ffca3a647c76e629e4ef45103
SaretMagnoslove <magnoslove@gmail.com>
2018-07-11 00:08:18 +0300

    Add lesson11Code

--- 10_Multiprocessing/lesson10Code.py
+++ 10_Multiprocessing/lesson10Code.py
@@ -7,6 +7,7 @@ def spawn(num):
 
 if __name__ == '__main__':
     for i in range(5):
-        p = multiprocessing.Process(target=spawn, args=(i,))
+        p = multiprocessing.Process(target=spawn, args=(i, ))
         p.start()
-        p.join() # wait for process to finish (running in order - if processes are depended)
+        p.join(
+        )  # wait for process to finish (running in order - if processes are depended)
